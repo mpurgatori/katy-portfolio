3e0499ce4cbe7af67ce3357f6b0d77647134e7ab
mpurgatori <mpurgatori@gmail.com>
2020-08-18 00:58:07 +0100

    Updates

---
+++ precache-manifest.10d59f3573260513bcc37b3c21eb60cb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32cda270db2311efda70f6b5f8b8924",
+    "url": "/katy-portfolio/index.html"
+  },
+  {
+    "revision": "55c2d07cd5aa03981122",
+    "url": "/katy-portfolio/static/js/2.35d2f76c.chunk.js"
+  },
+  {
+    "revision": "6a2bc4dfc852217704ff5945e37d112d",
+    "url": "/katy-portfolio/static/js/2.35d2f76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5de1a20cb27d0f6c2c",
+    "url": "/katy-portfolio/static/js/main.f9533e32.chunk.js"
+  },
+  {
+    "revision": "575951856ad99f1854c6",
+    "url": "/katy-portfolio/static/js/runtime-main.f4fae641.js"
+  },
+  {
+    "revision": "29534bb02c13e2f423da151b8249b1cb",
+    "url": "/katy-portfolio/static/media/Athena_1.29534bb0.png"
+  },
+  {
+    "revision": "f3dec07d246bfad5bd1054c570152cd0",
+    "url": "/katy-portfolio/static/media/Athena_2.f3dec07d.png"
+  },
+  {
+    "revision": "81fe19eed6c9def5405805d00572e108",
+    "url": "/katy-portfolio/static/media/Athena_3.81fe19ee.png"
+  },
+  {
+    "revision": "2ad5389524f8c7fe9e3944cc5ba80fb8",
+    "url": "/katy-portfolio/static/media/Athena_4.2ad53895.png"
+  },
+  {
+    "revision": "997163d045c127aec3ce084da5b6557c",
+    "url": "/katy-portfolio/static/media/Athena_mockup_original.997163d0.png"
+  },
+  {
+    "revision": "4e4cee391106d0f197e3462c50d079f2",
+    "url": "/katy-portfolio/static/media/Brainiac_mockup_original.4e4cee39.png"
+  },
+  {
+    "revision": "5370844fc6fa4756e421fb66f8552253",
+    "url": "/katy-portfolio/static/media/Fixit_mockup_original.5370844f.png"
+  },
+  {
+    "revision": "8ca6f785a9ea84c1f596282a60274092",
+    "url": "/katy-portfolio/static/media/Katy_profile_ROTATED.8ca6f785.png"
+  },
+  {
+    "revision": "2d9ac0589a6b7ea8719a8cb0966ac140",
+    "url": "/katy-portfolio/static/media/Katy_profile_clips.2d9ac058.png"
+  },
+  {
+    "revision": "71fef336aff230ced1a2dd23d11d66f3",
+    "url": "/katy-portfolio/static/media/Underline_blue.71fef336.svg"
+  },
+  {
+    "revision": "23751cfb8d17a919feff1f1c5af1fba6",
+    "url": "/katy-portfolio/static/media/Underline_pink.23751cfb.svg"
+  },
+  {
+    "revision": "c4ed6ca1e0fc227e45749b476719ccaa",
+    "url": "/katy-portfolio/static/media/Underline_yellow.c4ed6ca1.svg"
+  }
+]);
